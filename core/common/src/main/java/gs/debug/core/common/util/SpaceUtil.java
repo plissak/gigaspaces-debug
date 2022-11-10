b953379cae3eeef53cfc6f3c93876d4080ef041b
Paul Lissak <plissak@skyroad.com>
2022-11-10 11:04:20 -0600

    add routing utils

--- core/common/src/main/java/gs/debug/core/common/util/SpaceUtil.java
+++ core/common/src/main/java/gs/debug/core/common/util/SpaceUtil.java
@@ -1,5 +1,7 @@
 package gs.debug.core.common.util;
 
+import org.openspaces.core.cluster.ClusterInfo;
+
 import com.gigaspaces.client.ReadModifiers;
 import com.gigaspaces.client.iterator.SpaceIteratorConfiguration;
 import com.gigaspaces.client.iterator.SpaceIteratorType;
@@ -15,5 +17,60 @@ public class SpaceUtil {
 		config.setReadModifiers(ReadModifiers.READ_COMMITTED); // this is how GSIterator behaved by default
 		return config;
 	}
+
+	/**
+	 * Returns true if the given routing key is applicable for the given partition (identified by the given cluster information).
+	 * Also returns true if there is no cluster or no routing key.
+	 * <p>
+	 * This method can be used for mapping non-partitioned space objects across partitioned PUs on a different space.
+	 *
+     * @param routingKey the data identifier where container routing is based on
+	 * @param clusterInfo the container's cluster information
+     * @return true if this container should include the corresponding data
+	 */
+    public static boolean accept(Long routingKey, ClusterInfo clusterInfo) {
+        if (routingKey == null || clusterInfo == null) {
+            return true;
+        }
+        return accept(routingKey, clusterInfo.getInstanceId(), clusterInfo.getNumberOfInstances());
+    }
+
+    /**
+	 * Returns true if the given routing key is applicable for the given container.
+	 * Also returns true if any of the given arguments are null.
+	 * <p>
+	 * This method can be used for mapping non-partitioned space objects across partitioned PUs on a different space.
+	 *
+     * @param routingKey the data identifier where container routing is based on
+     * @param containerNumber the number associated with this container (starting at 1)
+     * @param totalContainers the total number of containers we're distributing across
+     * @return true if this container should include the corresponding data
+     */
+    public static boolean accept(Long routingKey, Integer containerNumber, Integer totalContainers) {
+        if (routingKey == null || containerNumber == null || totalContainers == null) {
+            return true;
+        }
+        return (routingKey % totalContainers) == (containerNumber.intValue() - 1);
+    }
+
+	public static boolean isEvenContainer(ClusterInfo clusterInfo) {
+		Integer containerNumber = clusterInfo == null ? null : clusterInfo.getInstanceId();
+		if (containerNumber == null) {
+			return true;
+		}
+		return (containerNumber.intValue() - 1) % 2 == 0;
+	}
+
+	public static Long getRoutingKey(ClusterInfo clusterInfo) {
+		return clusterInfo == null ? 0L : getRoutingKey(clusterInfo.getInstanceId(), clusterInfo.getNumberOfInstances());
+	}
+
+    public static Long getRoutingKey(Integer containerNumber, Integer totalContainers) {
+        if (containerNumber == null || totalContainers == null) {
+            return 0L;
+        }
+        int routingKey = (containerNumber.intValue() - 1) % totalContainers;
+        return (long)routingKey;
+    }
 
 }
