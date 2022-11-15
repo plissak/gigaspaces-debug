ff70c12ae6e61f3f851789a8112c443bf723e28e
Paul Lissak <plissak@skyroad.com>
2022-11-15 10:52:00 -0600

    add authorities

--- core/server/src/main/java/gs/debug/core/server/security/DebugSecurityManager.java
+++ core/server/src/main/java/gs/debug/core/server/security/DebugSecurityManager.java
@@ -1,5 +1,7 @@
 package gs.debug.core.server.security;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Properties;
 
 import org.apache.log4j.Logger;
@@ -7,9 +9,15 @@ import org.apache.log4j.Logger;
 import com.gigaspaces.security.AccessDeniedException;
 import com.gigaspaces.security.Authentication;
 import com.gigaspaces.security.AuthenticationException;
+import com.gigaspaces.security.Authority;
 import com.gigaspaces.security.SecurityException;
 import com.gigaspaces.security.SecurityManager;
+import com.gigaspaces.security.authorities.MonitorAuthority;
+import com.gigaspaces.security.authorities.MonitorAuthority.MonitorPrivilege;
+import com.gigaspaces.security.authorities.SpaceAuthority;
+import com.gigaspaces.security.authorities.SpaceAuthority.SpacePrivilege;
 import com.gigaspaces.security.directory.DirectoryManager;
+import com.gigaspaces.security.directory.User;
 import com.gigaspaces.security.directory.UserDetails;
 
 public class DebugSecurityManager implements SecurityManager {
@@ -24,7 +32,28 @@ public class DebugSecurityManager implements SecurityManager {
 	@Override
 	public Authentication authenticate(UserDetails userDetails) throws AuthenticationException {
 		logger.debug("Authenticate[" + userDetails.getUsername() + "]: " + properties);
-		return new Authentication(userDetails);
+
+		List<Authority> list = new ArrayList<>();
+		list.add(new SpaceAuthority(SpacePrivilege.READ));
+		list.add(new SpaceAuthority(SpacePrivilege.EXECUTE));
+
+		list.add(new SpaceAuthority(SpacePrivilege.WRITE));
+		list.add(new SpaceAuthority(SpacePrivilege.TAKE));
+		list.add(new SpaceAuthority(SpacePrivilege.ALTER));
+
+		list.add(new MonitorAuthority(MonitorPrivilege.MONITOR_PU));
+		list.add(new MonitorAuthority(MonitorPrivilege.MONITOR_JVM));
+
+		Authority[] authorities = userDetails.getAuthorities();
+		if (authorities != null) {
+			for (Authority authority : authorities) {
+				if (authority != null && ! list.contains(authority)) {
+					list.add(authority);
+				}
+			}
+		}
+
+		return new Authentication(new User(userDetails.getUsername(), userDetails.getPassword(), list.toArray(new Authority[list.size()])));
 	}
 
 	@Override
